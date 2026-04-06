18b6607e62073e1e085f6b3cfabf9722c2cae9db
Mati-Costamagna <matias.costamagna@mi.unc.edu.ar>
2026-04-06 14:00:22 -0300

    Funciones en C

---
+++ fase1/gini.c
@@ -0,0 +1,10 @@
+#include "gini.h"
+
+// Fase 1: Implementación pura en C
+int float_to_int(float value) {
+    return (int)value;
+}
+
+int sumar_uno(int value) {
+    return value + 1;
+}
---
+++ fase1/gini.h
@@ -0,0 +1,8 @@
+#ifndef GINI_H
+#define GINI_H
+
+// Fase 1: Solo lógica en C
+int float_to_int(float gini);
+int sumar_uno(int gini);
+
+#endif
