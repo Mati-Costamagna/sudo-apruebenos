d5da2dfefb0593eacaca91e2d792e55c251e34e5
Mati-Costamagna <matias.costamagna@mi.unc.edu.ar>
2026-04-05 23:58:56 -0300

    Interfaz en C para puente entre Python y Assembler

--- gini.c
+++ gini.c
@@ -1,7 +1,13 @@
+#include "gini.h"
+
 int float_to_int(float value) {
-    return (int)value;
+    // Llamamos a la rutina en assembler pasando los dummies
+    // para forzar que 'value' viaje por el stack (15to argumento)
+    return asm_float_to_int(0,0,0,0,0,0, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0, value);
 }
 
 int sumar_uno(int value) {
-    return value + 1;
+    // Llamamos a la rutina en assembler pasando los dummies
+    // para forzar que 'value' viaje por el stack (7mo argumento)
+    return asm_sumar_uno(0,0,0,0,0,0, value);
 }
