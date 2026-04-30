ebfc10e0d2acdb8723f606dd35d72065addbb244
Pilar Sabena <pilar.sabena@mi.unc.edu.ar>
2026-04-30 07:07:11 -0300

    modifique el file para ver la image base

--- Parte A/hello.c
+++ Parte A/hello.c
@@ -1,17 +1,23 @@
 #include <efi.h>
 #include <efilib.h>
 
-EFI_STATUS //devuelve codigo exito o error
-EFIAPI //convencion de llamadas que usa UEFI
+EFI_STATUS
+EFIAPI
 efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
-    InitializeLib(ImageHandle, SystemTable); //inicializa gnu-file y system table
+    InitializeLib(ImageHandle, SystemTable);
 
-    Print(L"Hello, UEFI World!\n\r");
+    EFI_LOADED_IMAGE *li;
+    gBS->HandleProtocol(ImageHandle, &LoadedImageProtocol, (void**)&li);
+    UINTN base = (UINTN)li->ImageBase;
+    Print(L"Image base high: 0x%x\n\r", (UINT32)(base >> 32));
+    Print(L"Image base low:  0x%x\n\r", (UINT32)(base & 0xFFFFFFFF));
+
+    Print(L"Hello UEFI\n\r");
     Print(L"Presiona cualquier tecla para salir...\n\r");
 
     UINTN index;
-    gBS->WaitForEvent(1, &ST->ConIn->WaitForKey, &index); //espera a que el usuario presione una tecla
+    gBS->WaitForEvent(1, &ST->ConIn->WaitForKey, &index);
 
     return EFI_SUCCESS;
 }
