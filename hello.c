e9a20c0369580a56cb410332c151f5c3435b7544
Mati-Costamagna <matias.costamagna@mi.unc.edu.ar>
2026-05-25 13:57:48 -0300

    Introduccion TP5

--- hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() {
-    printf("Hola Sistemas de Computacion!\n");
-    return 0;
-}
-
--- segfault.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int *p = NULL;
-    *p = 42;   /* provoca SIGSEGV: escritura en dirección nula */
-    return 0;
-}
